ee0e9d00be15c06e8ffa0315d398633cdc64a45d
Julian Olarte <jolarte@fullstacklabs.co>
2021-12-08 09:26:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import Input from './components/input'
+import Button from './components/button'
+import Section from './components/section'
+import Balance from './components/balance'
+import Container from './components/container'
 
-function App() {
+const compoundInterest = (deposit = 0, contribution = 0, years = 0, rate = 0) => {
+  let total = deposit;
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+  return Math.round(total);
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+})
+
+const App = () => {
+  const [balance, setBalance] = useState('')
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const balance = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate));
+    setBalance(formatter.format(balance));
+  }
+
+  const formSchema = Yup.object({
+    deposit: Yup.number()
+      .required('Obligatorio')
+      .typeError('Debe ser un numero'),
+    contribution: Yup.number()
+      .required('Obligatorio')
+      .typeError('Debe ser un numero'),
+    years: Yup.number()
+      .required('Obligatorio')
+      .typeError('Debe ser un numero'),
+    rate: Yup.number()
+      .required('Obligatorio')
+      .typeError('Debe ser un numero')
+      .min(0, 'El valor minimo debe ser 0')
+      .max(1, 'El valor maximo debe ser 1')
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={formSchema}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form>
+            <Input name="deposit" label="Deposito Inicial" />
+            <Input name="contribution" label="Contribucion Anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interes Estimado" />
+            <Button type="submit">Calcular</Button>
+          </Form>
+        </Formik>
+        {balance !== '' ? <Balance>Balance final: {balance}</Balance> : ''}
+      </Section>
+    </Container>
   );
 }
 
---
+++ src/components/input.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { useField } from 'formik'
+
+const Control = styled.div`
+    margin-bottom: 20px;
+`
+
+const Label = styled.label`
+    color: black;
+    display: block;
+    margin-bottom: 5px;
+`
+
+const MyInput = styled.input`
+    outline: none;
+    padding: 8px;
+    border: 1px solid #b1b3b5;
+    border-radius: 4px;
+    width: 100%;
+    margin-bottom: 5px;
+`
+const ErrorMessage = styled.div`
+    color: red;
+`
+const Input = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <Control>
+            <Label>{label}</Label>
+            <MyInput {...field} {...props} />
+            {meta.touched && meta.error ? (<ErrorMessage>{meta.error}</ErrorMessage>) : ''}
+        </Control>
+    )
+}
+
+export default Input; 
